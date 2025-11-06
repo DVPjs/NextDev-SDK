709b90a20d629296fba70d007c7d25fc3a35a913
DVPanel <082011jg@gmail.com>
2025-11-05 22:25:56 -0500

    Add comments and licensing information to auth@main.js
    Added licensing and documentation comments to the auth@main.js file.

--- v1.00/auth@main.js
+++ v1.00/auth@main.js
@@ -1,1 +1,11 @@
+// NextDev Public ESM SDK
+// Developed by "road.js" and the "NextDev Team"
+// GO TO THE DOCS WEBSITE AT "https://docs.nxtjs.dev"
+// Origin: sdk.nxtjs.dev
+// Website: nxtjs.dev
+// Discord: discord.gg/UaN6geq8be
+//
+// All rights reserved © 2025 NextDev
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 import*as Auth from"./auth/index.js";try{globalThis.NextDevAuth=Auth}catch{}export default Auth;export const config=Auth.config;export const signup=Auth.signup;export const login=Auth.login;export const verifyEmail=Auth.verifyEmail;export const me=Auth.me;export const guard=Auth.guard;export const attachFormHandlers=Auth.attachFormHandlers;export const setUserToken=Auth.setUserToken;export const getUserToken=Auth.getUserToken;export const setTenant=Auth.setTenant;export const issueDomainToken=Auth.issueDomainToken;
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
